e3f53dc95cd052ca217b9db51b83f32d437befc9
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2022-07-21 22:33:09 -0500

    First commit

---
+++ home/views.py
@@ -0,0 +1,11 @@
+import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates,'
+)
+
+def index(request):
+    today =  datetime.datetime.now().date()
+    return render(request, "index.html", {"today": today})
